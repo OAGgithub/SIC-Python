0b141c50033f4992e638100aaaebe5c2a6c99d5c
OAGgithub <oniel.gg@gmail.com>
2024-07-11 16:20:22 -0400

    "Change"

--- DiagnosticCare/Model.py
+++ DiagnosticCare/Model.py
@@ -4,8 +4,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("./Python_Files/Dataset/Diseases_Training.csv")
-
+df = pd.read_csv("./DiagnosticCare/Dataset/Diseases_Training.csv")
+print(df.head())
 df1 = df.drop(["Unnamed: 133"],axis=1)
 
 sintomas = df1.drop('prognosis',axis=1)
@@ -20,7 +20,7 @@ pred_model = model.predict(x_test)
 
 print(classification_report(y_test,pred_model))
 
-df_test = pd.read_csv("./Python_Files/Dataset/Diseases_Testing.csv")
+df_test = pd.read_csv("./DiagnosticCare/Dataset/Diseases_Testing.csv")
 sintomas_2 = df_test.drop('prognosis', axis=1)
 pred_model2 = model.predict(sintomas_2)
 target_2 = df_test['prognosis']
