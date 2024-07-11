a6110ad5d3767949576fd7db7eb298e6aab5bbde
Oliver Abreu Mateo <132733631+revilo05@users.noreply.github.com>
2024-07-11 18:37:18 -0400

    Update app.py
    Mini cambios

--- FRONT/app/app.py
+++ FRONT/app/app.py
@@ -9,12 +9,12 @@ from models import EnfermedadesDF, obtener_temperatura, mapear_clima, buscar_enf
 app = Flask(__name__)
 
 # URL del dataset de enfermedades
-url_enfermedades = 'https://github.com/OAGgithub/SIC-Python/blob/main/Python_Files/Dataset/Disease_symptom_and_patient_profile_dataset_updated.xlsx?raw=true'
-df_enfermedades = pd.read_excel(url_enfermedades)
+url_enfermedades = 'https://github.com/OAGgithub/SIC-Python/blob/main/DiagnosticCare/Dataset/Diseases_Training.csv'
+df_enfermedades = pd.read_csv(url_enfermedades)
 
 # URL del dataset con información climática
-url_clima_enfermedades = 'https://github.com/OAGgithub/SIC-Python/blob/main/Python_Files/Dataset/Disease_symptom_and_patient_profile_dataset_with_weather.xlsx?raw=true'
-df_clima_enfermedades = pd.read_excel(url_clima_enfermedades)
+url_clima_enfermedades = 'https://github.com/OAGgithub/SIC-Python/blob/main/DiagnosticCare/Dataset/Diseases_Training.csv'
+df_clima_enfermedades = pd.read_csv(url_clima_enfermedades)
 
 # Instanciar la clase EnfermedadesDF para trabajar con los datos de enfermedades
 data = EnfermedadesDF(df_enfermedades)
