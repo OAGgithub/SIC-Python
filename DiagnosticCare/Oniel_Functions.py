955eba50c65902c42d1b6f5e2f31f2e5202a779d
OAGgithub <oniel.gg@gmail.com>
2024-07-11 18:30:26 -0400

    "Functions "

--- DiagnosticCare/Oniel_Functions.py
+++ DiagnosticCare/Oniel_Functions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-file_path = './DiagnosticCare/Dataset/Final_Disease_Weather_Dataset'
+file_path = './DiagnosticCare/Dataset/Final_Disease_Weather_Dataset.xlsx'
 df = pd.read_excel(file_path)
 
 class EnfermedadesDF:
@@ -8,28 +8,28 @@ class EnfermedadesDF:
         self.df = dataframe
 
     def search_by_name(self, name):
-        # Busca enfermedades por su nombre
-        return self.df[self.df['Disease'].str.contains(name, case=False, na=False)]
+        # Busca enfermedades por su nombre 
+        return self.df[self.df['Disease'].str.contains(name, case=False, na=False)][['Disease', 'Description']]
 
     def get_all(self):
-        
         # Selecciona todas las enfermedades
         return self.df['Disease'].unique()
 
     def select_number_of_diseases(self, names, number):
-        
-        # Selecciona un número de enfermedades por nombre.
+        # Selecciona un número de enfermedades por nombre 
         filtered_df = self.df[self.df['Disease'].isin(names)]
-        return filtered_df.head(number)
+        return filtered_df[['Disease', 'Recommendations']].head(number)
 
     def filter_diseases(self, **filters):
-        
-        # Filtra enfermedades basado en criterios específicos
+        # Filtra enfermedades basado en criterios específicos y retorna solo las columnas "Disease" y "Recommendations"
         filtered_df = self.df.copy()
         for key, value in filters.items():
             if key in filtered_df.columns:
                 filtered_df = filtered_df[filtered_df[key] == value]
-        return filtered_df
+        return filtered_df[['Disease', 'Recommendations']]
+
+    def get_recommendations(self):
+        return self.df[['Disease', 'Recommendations']]
 
 
 data = EnfermedadesDF(df)
@@ -51,3 +51,4 @@ print(disease_name_search.head())
 print(all_diseases)
 print(selected_diseases)
 print(filtered_diseases.head())
+print(data.get_recommendations())
